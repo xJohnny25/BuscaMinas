471d135492cf7901c667d65285770c479fd9fa9c
Jonathan Perez <thejonathan38a10@gmail.com>
2024-01-21 21:14:44 +0100

    Prueba commit BuscaMinas

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[][] tablero = new int[4][5];
+
+        int numero_minas = 0;
+        int tesoro;
+
+
+
+        do {
+            for (int i = 0; i < tablero.length; i++){
+                for (int j = 0; j < tablero[0].length; j++){
+
+                }
+            }
+        }while (numero_minas != 3);
+
+
+    }
+}
